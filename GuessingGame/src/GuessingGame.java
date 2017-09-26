6703fd96c4ef69cd15fdd04dc4e7d758ea0abdc6
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2017-09-26 12:59:17 -0400

    First Code

---
+++ GuessingGame/src/GuessingGame.java
@@ -0,0 +1,15 @@
+/*
+ * Sidney Phillips
+ * 9.26.17
+ * Guessing Game Project
+ */
+public class GuessingGame {
+	//initialize function, calls game function
+	public static void main(String args[]) {
+		guessGame();
+	}
+	//runs the game
+	public static void guessGame() {
+		
+	}
+}
